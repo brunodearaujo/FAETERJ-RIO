4e4be559328ad3fbc0f008745dc7d5bb92ed053e
Bruno de Araujo <64059231+brunodearaujo@users.noreply.github.com>
2025-02-17 23:14:06 +0000

    tópicos abordados na `Aula #01` de FPR

---
+++ FPR/revisao.c
@@ -0,0 +1,17 @@
+/*
+operadores:unários, binários, ternários; "?"
+tomada de decisão
+estrutura de repetição
+funções gay
+ponteiros
+switch case
+parâmetro por valor
+parâmetro por referência
+retornos booleanos
+vetores, deslocação por vetores e substituição de valores
+
+geralmente: tempo execução > memória (mas não é pra extrapolar)
+
+apresentação de matrizes
+
+*/
